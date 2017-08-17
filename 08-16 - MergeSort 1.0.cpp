5401bc01eba832d0d8ad5bf2c33369219d7ad06f
Matheus Gomes <matheusribeirogomes@hotmail.com>
2017-08-16 21:27:27 -0300

    16/08/2017

---
+++ 08-16 - MergeSort 1.0.cpp
@@ -0,0 +1,66 @@
+#include <windows.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define tam 1000
+
+int vetor[tam], vetorOld[tam];
+int scratch[tam];
+
+void gera(){
+	int i;
+	srand (time(NULL));
+	for(i=0; i<tam; i++) vetor[i] = rand();
+	for(i=0; i<tam; i++) vetorOld[i] = vetor[i];
+}
+
+void mergeSort(int lo, int hi){
+    int mid, l, h, k;
+    if(lo < hi){
+            mid = (lo + hi)/2;
+            mergeSort(lo, mid);
+            mergeSort(mid+1, hi);
+            l = lo;
+            h = mid+1;
+            for(k=lo; k<=hi; k++){
+                if(l<=mid && (h > hi || vetor[l] < vetor[h])){
+                    scratch[k] = vetor[l];
+                    l++;
+                } else {
+                    scratch[k] = vetor[h];
+                    h++;
+                }
+            }
+            for(k=lo; k<=hi; k++){
+                vetor[k] = scratch[k];
+            }
+    }
+}
+
+
+void imprime(){
+	for(int i=0; i<tam; i++){
+		printf(" %d\t  ||   %d\t", vetorOld[i], vetor[i]);
+	}
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	clock_t tempo1, tempo2;
+	gera();
+	tempo1 = clock();
+	mergeSort(0, tam-1);
+	tempo2 = clock() - tempo1;
+	if(tam<=291){
+		printf(" Original ||   Ordenado   ||  Índice\n\t  ||\t\t  ||\t\t\n");
+		printf("================================================\n");
+		imprime();
+	} else {
+		imprime();
+		printf("================================================\n");
+		printf("\t  ||\t\t  ||\n Original ||   Ordenado   ||  Índice\n\t  \n");
+	}
+	printf("\n>> Tempo de operação: %.4f\n", (float) tempo2/CLOCKS_PER_SEC);
+}
